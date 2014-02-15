18ed5b62f2520c84504c5a3ae4e43ce2c46bb4b8
Sigtryggur <sigtrygg@mit.edu>
2014-02-15 14:16:57 -0500

    Init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heartbeat::Application.config.secret_token = '24fa09e3dccbd7f996ac304bf15322f6fd4ab3af429353c38a605e12b825266518108ff94f7b787206dfa34bd12965ad50f83dec4be9776098ff9f7e7967aafd'
